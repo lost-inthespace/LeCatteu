c1e9206494b0cc6d298d16a76299c8c716b5f67d
Mohammad Zmzmi <91334712+lost-inthespace@users.noreply.github.com>
2023-02-27 20:13:23 +0300

    Delete content directory

--- content/question-answers.js
+++
@@ -1,7 +0,0 @@
-const questions = [
-    { question: "What is My Favourite Candy?", answer: "Cotton Candy" },
-    { question: "What is the largest planet in our solar system?", answer: "Jupiter" },
-    { question: "Who painted the Mona Lisa?", answer: "Leonardo da Vinci" },
-    { question: "What is the symbol for gold on the periodic table?", answer: "Au" },
-    { question: "Who wrote To Kill a Mockingbird?", answer: "Harper Lee" }
-];
